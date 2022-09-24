d4889041c215150d5186747b36636af0c4115a9d
Sayem92 <sayemmiha123@gmail.com>
2022-09-24 11:43:42 +0600

    add LS

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,15 +1,25 @@
 // use local storage to manage cart data
 
 const addToDb = (id)=>{
-    const quantity = localStorage.getItem(id)
+    let shoppingCart = {} ;
+
+    // get the shopping cart
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart)
+    }
+
+    //add quantity
+    const quantity = shoppingCart[id]
     if(quantity){
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity)
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity
     }
     else{
-        localStorage.setItem(id, 1)
+        shoppingCart[id] = 1
     }
     
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
 }
 
 export {addToDb}
